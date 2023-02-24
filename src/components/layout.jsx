c5f6b5375bbe82d16d8a540669121b1d504122c0
razorhollow <rob@razorhollow.com>
2023-02-24 12:06:20 -0500

    add responsive nav bar and header section

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,16 +5,18 @@ import { StaticImage } from 'gatsby-plugin-image'
 const Layout = ({ pageTitle, children }) => {
     return (
             <div className={container}>
-                <nav>
-                    <ul className={navLinks}>
-                        <li className={navLinkItem}><Link to="/" className={navLinkText}><StaticImage src='../images/rh.png' className={logo}/></Link></li>
-                        <li className={navLinkItem}><Link to="/work" className={navLinkText}>Work</Link></li>
-                        <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-                        <li className={navLinkItem}><Link to="/expertise" className={navLinkText}>Services</Link></li>
-                        <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
-                        <li className={navLinkItem}><Link to="/contact" className={navLinkText}>Contact</Link></li>
-                    </ul>
-                </nav>
+                <header>
+                    <Link to="/" className={navLinkText}><StaticImage src='../images/rh.png' className={logo}/></Link>
+                    <nav>
+                        <ul className={navLinks}>
+                            <li className={navLinkItem}><Link to="/work" className={navLinkText}>Work</Link></li>
+                            <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                            <li className={navLinkItem}><Link to="/expertise" className={navLinkText}>Services</Link></li>
+                            <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+                            <li className={navLinkItem}><Link to="/contact" className={navLinkText}>Contact</Link></li>
+                        </ul>
+                    </nav>
+                </header>
                 <main>
                     <h1>{pageTitle}</h1>
                     {children}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
+import {backgroundImage, container, content} from './index.module.css'
 
 
 
@@ -7,6 +9,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <h1>You've landed on the home page</h1>
+      <div className={container}>
+        <h2 className={content}>Work</h2>
+        <StaticImage className="backgroundImage" style={{ position: "absolute" }} src="../images/into-sun.jpg" alt="" />
+      </div>
     </Layout>
   )
 }
